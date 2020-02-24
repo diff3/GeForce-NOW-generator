e45777e671a9f698371f07ec7828022b86b722cb
diff3 <171053+diff3@users.noreply.github.com>
2020-02-24 09:35:01 +0100

    first update

--- GeForce_NOW_games.py
+++ GeForce_NOW_games.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import argparse
+"""
+A Small script to download and compare game list from GeForce Now and
+a users Steam gaming list. Do all configs in config.ini
+"""
+
 import configparser
 import json
 import urllib.request
@@ -8,66 +12,107 @@ import steamapi
 
 
 def get_json(url_json):
+    """
+    Download json file from internet
+    return json
+    """
     with urllib.request.urlopen(url_json) as url:
         data = json.loads(url.read().decode())
     return data
 
 
 def save_file(data, file_name):
+    """
+    Save a list to harddisk, one game per line
+    """
+
     with open(file_name, 'w', encoding='utf8') as txt_file:
         for item in data:
             txt_file.write("%s\n" % item)
 
 
 def generate_list(json_data):
-    data = [game['title'] for game in json_data]
+    """
+    Generates a Geforce list of game, also remove special characters for easier
+    comparison.
+
+    return list
+    """
+
+    global whitelist
+
+    data = [''.join(filter(whitelist.__contains__,game['title'])) for game in json_data]
     return data
 
 
 def connect(api_key, userurl):
+    """
+    Downloads a list of games from Steam using it's API. Also removes special
+    characters for easier comparison.
+
+    return list
+    """
+
+    global whitelist
+
     steamapi.core.APIConnection(api_key=api_key, validate_key=True)
     user = steamapi.user.SteamUser(userurl=userurl)
     games = user.games
-    games_list = [game.name for game in games]
+    games_list = [''.join(filter(whitelist.__contains__,game.name)) for game in games]
     return games_list
 
 
 def get_common(geforce_game_list, steam_game_list):
+    """
+    Comparies both lists and return a list with supported games
+    """
+
     result = [game for game in steam_game_list if game in geforce_game_list]
     return result
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog='GeForce-NOW-games',
-        description='Generate a list of owned Steam games that work on '
-                    'GeForce NOW platform.')
-    parser.add_argument(
-        'username', metavar='username', type=str,
-        help='Steam username (urluser - present in the profile url)')
-    parser.add_argument(
-        'filename', metavar='filename', type=str,
-        help='name of the output file - will be created or overwritten')
-    args = parser.parse_args()
+    print("GeForce_NOW_games: Starting")
 
+    # Config
+    print("Loading configs")
     config = configparser.ConfigParser()
     config.read('config.ini')
+
     key = config['STEAM']['ApiKey']
+    user = config['STEAM']['User']
+    geforce_url = config['GEFORCE']['url']
+    geforce = config['OUTPUT']['geforce']
+    steam = config['OUTPUT']['steam']
+    supported = config['OUTPUT']['supported']
+    whitelist = set(config['WHITELIST']['list'])
 
-    geforce_url = \
-        'https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json'
-    geforce_list = get_json(geforce_url)
-    game_list = generate_list(geforce_list)
+    # GeForce Now
+    print("Generating GeForce Now list")
+    game_list = get_json(geforce_url)
+    geforce_list = generate_list(game_list)
+    geforce_list.sort()
 
+    # Steam API
     try:
-        steam_list = connect(key, args.username)
+        print("Generating Steam list")
+
+        steam_list = connect(key, user)
+        steam_list.sort()
     except steamapi.errors.APIException:
         print("Couldn't get the game list from Steam.")
         input("Press Enter to exit...")
         exit(1)
 
-    result_data = get_common(game_list, steam_list)
-    save_file(result_data, args.filename)
+    # Result
+    print("Compare lists")
+    result_data = get_common(geforce_list, steam_list)
+    result_data.sort()
 
-    print("Done! :D")
-    input("Press Enter to exit...")
+    # Save to files
+    print("Saving lists")
+    save_file(geforce_list, geforce)
+    save_file(steam_list, steam)
+    save_file(result_data, supported)
+
+    print("Done!")
